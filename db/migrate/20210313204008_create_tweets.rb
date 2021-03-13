55ed00523690ce3d67a827466ab3348b2ec03829
rcbdelarosa <rcb.delarosa@gmail.com>
2021-03-13 15:49:50 -0600

    populated migration file for tweets

--- db/migrate/20210313204008_create_tweets.rb
+++ db/migrate/20210313204008_create_tweets.rb
@@ -1,7 +1,14 @@
 class CreateTweets < ActiveRecord::Migration[6.1]
   def change
     create_table :tweets do |t|
-
+      t.integer :id
+      t.integer :user_id
+      t.string :user
+      t.text :content
+      t.string :media_image
+      t.integer :likes
+      t.integer :comments
+      t.integer :retweets
       t.timestamps
     end
   end
