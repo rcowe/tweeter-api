c1dd0e8bf0dc7aab4c057d4b38ed4b7c5ec2a411
rcbdelarosa <rcb.delarosa@gmail.com>
2021-03-13 15:53:04 -0600

    populated users migration file

---
+++ db/migrate/20210313215015_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.integer :id
+      t.string :image
+      t.string :username
+      t.string :location
+      t.string :bio
+      t.string :create_date
+      t.integer :followers
+      t.integer :following
+      t.timestamps
+    end
+  end
+end
