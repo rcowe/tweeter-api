69f16faecae1ddb083714e07810563ebdf3e31dc
rcbdelarosa <rcb.delarosa@gmail.com>
2021-03-13 16:31:29 -0600

    created foreign key files

--- db/migrate/20210313215015_create_users.rb
+++ db/migrate/20210313215015_create_users.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.integer :id
-      t.integer :user_id
       t.string :image
+      t.string :name
       t.string :username
       t.string :location
       t.string :bio
--- db/migrate/20210313215339_create_comments.rb
+++ db/migrate/20210313215339_create_comments.rb
@@ -1,8 +1,6 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.integer :id
-      t.integer :tweet_id
       t.string :username
       t.string :content
       t.timestamps
