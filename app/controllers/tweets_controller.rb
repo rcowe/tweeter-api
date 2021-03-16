5c5760c8bf3f86b34fda65e6374c86d302479764
rcbdelarosa <rcb.delarosa@gmail.com>
2021-03-16 01:02:42 -0500

    Commit 6: All Tweets are visible at the index route

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,17 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
+    render json: @tweets
   end
 
   def show
+    render json: @tweet
+  end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
 end
