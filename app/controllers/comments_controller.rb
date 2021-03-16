bbcb5f4dc2348d358aab3994438ccc2265e9e878
rcbdelarosa <rcb.delarosa@gmail.com>
2021-03-16 01:05:06 -0500

    Commit 6: All Comments are visible at the index route

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,17 @@
 class CommentsController < ApplicationController
   def index
+    @comments = Comment.all
+    render json: @comments
   end
 
   def show
+    render json: @comments
+  end
+
+  private
+
+  def set_tweet
+    @comment = Comment.find(params[:id])
   end
 
 end
